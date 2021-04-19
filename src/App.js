770340a04fd079a0e83212ecd28a2a29273b2b29
gcmartin <gscalica@gmail.com>
2021-04-19 16:14:11 -0700

    Create ThemeContext

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Navbar from "./containers/Navbar";
 import Form from "./containers/Form";
+import PageContent from "./containers/PageContent";
+import { ThemeProvider } from "./contexts/ThemeContext";
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Form />
-    </> 
+    <ThemeProvider>
+      <PageContent>
+        {/* Navbar and Form will be part of "props.children" */}
+        <Navbar />
+        <Form />
+      </PageContent>
+    </ThemeProvider>
   );
 }
 
---
+++ src/contexts/ThemeContext.js
@@ -0,0 +1,18 @@
+import React, { Component, createContext } from "react";
+
+export const ThemeContext = createContext();
+
+export class ThemeProvider extends Component {
+  state = {
+    isDarkMode: true
+  }
+
+  render() {
+    return (
+      <ThemeContext.Provider value={{ ...this.state, tastesLikeChicken: true }}>
+        {this.props.children}
+      </ThemeContext.Provider>
+    );
+  }
+}
+
