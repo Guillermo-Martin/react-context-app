03f2a1c1020a78ca3469a451b3187b8b97c5862a
gcmartin <gscalica@gmail.com>
2021-04-20 14:50:36 -0700

    Create LanguageContext

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import Navbar from "./containers/Navbar";
 import Form from "./containers/Form";
 import PageContent from "./containers/PageContent";
 import { ThemeProvider } from "./contexts/ThemeContext";
+import { LanguageProvider } from "./contexts/LanguageContext";
 
 function App() {
   return (
     <ThemeProvider>
-      <PageContent>
-        {/* Navbar and Form will be part of "props.children" */}
-        <Navbar />
-        <Form />
-      </PageContent>
+      <LanguageProvider>
+        <PageContent>
+          
+          {/* Navbar and Form will be part of "props.children" */}
+          <Navbar />
+          <Form />
+        
+        </PageContent>
+      </LanguageProvider>
     </ThemeProvider>
   );
 }
