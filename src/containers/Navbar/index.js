4a13d4a560376660ae8834506e9b533e01f08e5a
gcmartin <gscalica@gmail.com>
2021-04-20 16:35:33 -0700

    Create 'withLanguageContext' higher order component

--- src/containers/Navbar/index.js
+++ src/containers/Navbar/index.js
@@ -9,6 +9,22 @@ import Switch from "@material-ui/core/Switch";
 import { withStyles } from "@material-ui/core/styles";
 import styles from "./../../assets/styles/NavbarStyles";
 import { ThemeContext } from "./../../contexts/ThemeContext";
+import { withLanguageContext } from "./../../contexts/LanguageContext";
+
+const content = {
+  english: {
+    search: "Search",
+    flag: "🇬🇧"
+  },
+  french: {
+    search: "Chercher",
+    flag: "🇫🇷"
+  },
+  spanish: {
+    search: "Buscar",
+    flag: "🇪🇸"
+  }
+}
 
 class Navbar extends Component {
   // tell the Navbar component to see if it's nested inside of a "ThemeContext" provider
@@ -19,34 +35,37 @@ class Navbar extends Component {
     // console.log(this.context);
 
     const { isDarkMode, toggleTheme } = this.context;
-
     const { classes } = this.props;
-
+    const { language } = this.props.languageContext;
+    const { search, flag } = content[language];
+ 
     return (
-      <div className={classes.root}>
-        <AppBar position="static" color={isDarkMode ? "default" : "primary"}>
-          <Toolbar>
-            <IconButton className={classes.menuButton} color="inherit">
-              <span>🇫🇷</span>
-            </IconButton>
-            <Typography className={classes.title} variant="h6" color="inherit">App Title</Typography>
-            <Switch onChange={toggleTheme} />
-            <div className={classes.grow} />
-            <div className={classes.search}>
-              <div className={classes.searchIcon}>
-                <SearchIcon />
-              </div>
-              <InputBase placeholder="Search..." classes={{
-                  root: classes.inputRoot,
-                  input: classes.inputInput
-                }}
-              />
-            </div>
-          </Toolbar>
-        </AppBar>
-      </div>
+      
+          <div className={classes.root}>
+            <AppBar position="static" color={isDarkMode ? "default" : "primary"}>
+              <Toolbar>
+                <IconButton className={classes.menuButton} color="inherit">
+                  <span>{flag}</span>
+                </IconButton>
+                <Typography className={classes.title} variant="h6" color="inherit">App Title</Typography>
+                <Switch onChange={toggleTheme} />
+                <div className={classes.grow} />
+                <div className={classes.search}>
+                  <div className={classes.searchIcon}>
+                    <SearchIcon />
+                  </div>
+                  <InputBase placeholder={`${search}...`} classes={{
+                      root: classes.inputRoot,
+                      input: classes.inputInput
+                    }}
+                  />
+                </div>
+              </Toolbar>
+            </AppBar>
+          </div>
+
     );
   }
 }
 
-export default withStyles(styles)(Navbar);
+export default withLanguageContext(withStyles(styles)(Navbar));
--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -19,3 +19,16 @@ export class LanguageProvider extends Component {
     );
   }
 }
+
+// defining our own higher order component
+// 1. it expects a "Component" (an entire component), with some props
+// 2. it then returns a "LanguageContext.Consumer" which expects a function as its child
+// 3. this function takes a value from the "LanguageContext" (the "Context.Provider" from above)
+// 4. whatever that value is, we're going to take the component that was passed in (which is now wrapped in the LanguageContext.Consumer tags)
+// and return that same component with a new prop in it (in this case, "languageContext")
+// 5. then pass in all other props
+export const withLanguageContext = Component => props => (
+  <LanguageContext.Consumer>
+    {value => <Component languageContext={value} {...props} />}
+  </LanguageContext.Consumer>
+)
