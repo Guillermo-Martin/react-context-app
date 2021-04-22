e111a72a0e5e358f23df767d5e51e7e7a021ac65
gcmartin <gscalica@gmail.com>
2021-04-21 17:17:44 -0700

    Convert PageContent into functional component and use hooks

--- src/containers/PageContent/index.js
+++ src/containers/PageContent/index.js
@@ -1,26 +1,52 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
 import { ThemeContext } from "./../../contexts/ThemeContext";
 
-class PageContent extends Component {
-  // set the context for this component
-  static contextType = ThemeContext;
+function PageContent(props) {
+  // grab the data from the context
+  const { isDarkMode } = useContext(ThemeContext);
 
-  render() {
-    // grab the data from the context
-    const { isDarkMode } = this.context;
-
-    const styles = {
-      backgroundColor: isDarkMode ? "black" : "white",
-      height: "100vh",
-      width: "100vw"
-    }
-
-    return (
-      <div style={styles}>
-        {this.props.children}
-      </div>
-    );
+  const styles = {
+    backgroundColor: isDarkMode ? "black" : "white",
+    height: "100vh",
+    width: "100vw"
   }
+
+  return (
+    <div style={styles}>
+      {props.children}
+    </div>
+  );
 }
 
 export default PageContent;
+
+
+
+
+// ===== CLASS BASED COMPONENT =====
+// import React, { Component } from "react";
+// import { ThemeContext } from "./../../contexts/ThemeContext";
+
+// class PageContent extends Component {
+//   // set the context for this component
+//   static contextType = ThemeContext;
+
+//   render() {
+//     // grab the data from the context
+//     const { isDarkMode } = this.context;
+
+//     const styles = {
+//       backgroundColor: isDarkMode ? "black" : "white",
+//       height: "100vh",
+//       width: "100vw"
+//     }
+
+//     return (
+//       <div style={styles}>
+//         {this.props.children}
+//       </div>
+//     );
+//   }
+// }
+
+// export default PageContent;
