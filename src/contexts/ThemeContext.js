849f29d59372599687ba3d3e8bc3a505cd542e87
gcmartin <gscalica@gmail.com>
2021-04-21 17:01:17 -0700

    Create useToggleState and convert ThemeContext into a functional component with hooks

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,22 +1,40 @@
-import React, { Component, createContext } from "react";
+import React, { createContext } from "react";
+import useToggleState from "./../hooks/useToggleState";
 
 export const ThemeContext = createContext();
 
-export class ThemeProvider extends Component {
-  state = {
-    isDarkMode: false
-  }
-
-  toggleTheme = () => {
-    this.setState({ isDarkMode: !this.state.isDarkMode });
-  }
-
-  render() {
-    return (
-      <ThemeContext.Provider value={{...this.state, toggleTheme: this.toggleTheme}}>
-        {this.props.children}
-      </ThemeContext.Provider>
-    );
-  }
+export function ThemeProvider(props) {
+  const [isDarkMode, toggleTheme] = useToggleState(false)
+  
+  return (
+    <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
+      {props.children}
+    </ThemeContext.Provider>
+  );
 }
 
+
+
+
+// ===== CONTEXT AS A CLASS-BASED COMPONENT =====
+// import React, { Component, createContext } from "react";
+
+// export const ThemeContext = createContext();
+
+// export class ThemeProvider extends Component {
+//   state = {
+//     isDarkMode: false
+//   }
+
+//   toggleTheme = () => {
+//     this.setState({ isDarkMode: !this.state.isDarkMode });
+//   }
+
+//   render() {
+//     return (
+//       <ThemeContext.Provider value={{...this.state, toggleTheme: this.toggleTheme}}>
+//         {this.props.children}
+//       </ThemeContext.Provider>
+//     );
+//   }
+// }
